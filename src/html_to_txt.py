53532bc02ebec98066976221065fb9719f4a01ee
Yuudai Kawakami <kwkm.k7.b.s.l.f@gmail.com>
2020-08-21 16:50:38 +0900

    ファイルの作成処理を追加

--- src/html_to_txt.py
+++ src/html_to_txt.py
@@ -17,16 +17,16 @@ def html2text(r_file, w_dir):
     # テキストの読み込み
     with open(r_file) as f:
         html = f.read()
-
     # テキストの取得
     soup = BeautifulSoup(html,"html.parser")
     text = soup.get_text()
-
+    # 書き込みディレクトリが存在しない場合は作成
+    if not os.path.isdir(w_dir): os.mkdir(w_dir)
     # テキストの書き込み
     basename = os.path.basename(r_file)  # 読み込みファイル名を取得
     name = os.path.splitext(basename)[0]  # 拡張子を削除
-    w_file = w_dir + name + '.txt'
-    with open(w_file, mode='w') as f:
+    w_path = w_dir + name + '.txt'
+    with open(w_path, mode='w') as f:
         f.write(text)
 
 def roop_html2text(r_dir, w_dir):
@@ -37,13 +37,26 @@ def roop_html2text(r_dir, w_dir):
         r_dir (str): HTMLファイルが格納されたディレクトリのパス
         w_dir (str): テキストファイルを保存するディレクトリのパス
     """
-    # 各HTMLファイルからテキストを取得し、保存する。
-    filepaths = utils.get_filepaths(r_dir)
-    for r_file in filepaths:
-        html2text(r_file, w_dir)
+    # フォルダの存在を確認
+    if os.path.isdir(r_dir):
+        # ファイルパスの取得
+        filepaths = utils.get_filepaths(r_dir)
+        # 各HTMLファイルからテキストを取得し、保存する。
+        if len(filepaths) == 0:
+            print('ファイルが存在しません')
+        else:
+            for r_file in filepaths:
+                html2text(r_file, w_dir)
+    else:
+        print('{0} というフォルダは存在しません'.format(r_dir))
 
 if __name__ == '__main__':
-
-    r_dir = './data/yuho_sample/'
-    w_dir = './data/text_sample/'
+    
+    r_dir = './data/yuho_sample/2019/'
+    w_dir = './data/text_sample/2019/'
     roop_html2text(r_dir, w_dir)
+
+    # sample
+    # r_dir = './data/yuho_sample/2019/'
+    # w_dir = './data/text_sample/2019/'
+    # roop_html2text(r_dir, w_dir)
