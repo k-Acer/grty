866da9f1e1073898531aae495bc74f7bc7b8e876
Yuudai Kawakami <kwkm.k7.b.s.l.f@gmail.com>
2020-08-25 10:13:11 +0900

    読み込み処理の変更

--- src/html_to_txt.py
+++ src/html_to_txt.py
@@ -6,14 +6,15 @@ import chardet
 import utils
 
 
-def html2text(r_file, w_dir):
+def html2text(r_file):
     """
-    htm(html)ファイルからテキストデータを取得し、
-    テキストデータをテキストファイルに保存する。
+    htm(html)ファイルからテキストデータを取得する。
 
     Args:
         r_file (str): HTMLファイルが格納されたファイルのパス
-        w_dir (str): テキストファイルを保存するディレクトリのパス
+
+    Returns:
+        text (str): テキストデータ
     """
     # テキストをバイト列として読み込み
     with open(r_file, 'rb') as f:
@@ -24,45 +25,90 @@ def html2text(r_file, w_dir):
     else:
         html.decode('cp932')
     # テキストの取得
-    soup = BeautifulSoup(html,"html.parser")
+    soup = BeautifulSoup(html, "html.parser")
     text = soup.get_text()
-    # 書き込みディレクトリが存在しない場合は作成
-    if not os.path.isdir(w_dir): os.mkdir(w_dir)
-    # テキストの書き込み
-    basename = os.path.basename(r_file)  # 読み込みファイル名を取得
-    name = os.path.splitext(basename)[0]  # 拡張子を削除
-    w_path = w_dir + name + '.txt'
-    with open(w_path, mode='w', encoding='utf-8') as f:
-        f.write(text)
+    return text
 
-def roop_html2text(r_dir, w_dir):
+def html2text_year(base_dir, year):
     """
-    html2text を繰り返し実行する。
+    １年分
 
     Args:
-        r_dir (str): HTMLファイルが格納されたディレクトリのパス
-        w_dir (str): テキストファイルを保存するディレクトリのパス
+        base_dir (str): ベースとなるパス
+        year (str): 年度
     """
-    # フォルダの存在を確認
-    if os.path.isdir(r_dir):
-        # ファイルパスの取得
-        filepaths = utils.get_filepaths(r_dir)
-        # 各HTMLファイルからテキストを取得し、保存する。
-        if len(filepaths) == 0:
-            print('ファイルが存在しません')
-        else:
-            for r_file in filepaths:
-                html2text(r_file, w_dir)
-    else:
-        print('{0} というフォルダは存在しません'.format(r_dir))
+    yuho_dir = os.path.join(base_dir, 'yuho')
+    # テキスト保存ディレクトリが存在しない場合は作成する。
+    text_dir = os.path.join(base_dir, 'text') 
+    if not os.path.isdir(text_dir): os.mkdir(text_dir)
+    # yuhoディレクトリ下の各年度ディレクトリ名のリストを取得
+    year_dir = os.path.join(yuho_dir, year)
+    # 全htmlファイルパスを取得
+    filepaths = []
+    f = utils.get_filepaths(year_dir)
+    for filepath in f:
+        filepaths.append(filepath)
+    # htmlファイルパスをテキストファイルに変換し、テキストファイルに保存
+    for filepath in filepaths:
+        # htmlファイルからテキストを取得
+        text = html2text(filepath)
+        # 書き込みファイル名の作成
+        basename = os.path.basename(filepath)  # 読み込みファイル名を取得
+        name = os.path.splitext(basename)[0]  # 拡張子を削除
+        w_dir = os.path.dirname(filepath).replace('yuho', 'text')  # 書き込みディレクトリ名を作成
+        # 書き込みディレクトリが存在しない場合は作成する。
+        if not os.path.isdir(w_dir): os.mkdir(w_dir)
+        w_file = os.path.join(w_dir, name + '.txt')  # 書き込みファイル名
+        # # テキストファイルに保存
+        with open(w_file, mode='w', encoding='utf-8') as f:
+            f.write(text)
+
+def html2text_all(r_dir):
+    """
+    指定されたパス以下にある yuhoディレクトリを取得する。
+    yuhoディレクトリ下にある年度ディレクトリを取得する。
+    各年度ディレクトリ下の全htmlファイルをテキストファイルに変換する。
+    変換したテキストファイルを '指定されたパス/text'フォルダ下に保存する。
+
+    Args:
+        r_dir (str): 読み込みディレクトリのパス
+    """
+    yuho_dir = os.path.join(r_dir, 'yuho')
+    # テキスト保存ディレクトリが存在しない場合は作成する。
+    text_dir = os.path.join(r_dir, 'text') 
+    if not os.path.isdir(text_dir): os.mkdir(text_dir)
+    # yuhoディレクトリ下の各年度ディレクトリ名のリストを取得
+    dirpaths = os.listdir(yuho_dir)
+    # 全htmlファイルパスを取得
+    filepaths = []
+    for d in dirpaths:
+        year_dir = os.path.join(yuho_dir, d)
+        f = utils.get_filepaths(year_dir)
+        for filepath in f:
+            filepaths.append(filepath)
+    # htmlファイルパスをテキストファイルに変換し、テキストファイルに保存
+    for f in filepaths:
+        # htmlファイルからテキストを取得
+        text = html2text(f)
+        # 書き込みファイル名の作成
+        basename = os.path.basename(f)  # 読み込みファイル名を取得
+        name = os.path.splitext(basename)[0]  # 拡張子を削除
+        w_dir = os.path.dirname(f).replace('yuho', 'text')  # 書き込みディレクトリ名を作成
+        # 書き込みディレクトリが存在しない場合は作成する。
+        if not os.path.isdir(w_dir): os.mkdir(w_dir)
+        w_file = os.path.join(w_dir, name + '.txt')  # 書き込みファイル名
+        # テキストファイルに保存
+        with open(w_file, mode='w', encoding='utf-8') as f:
+            f.write(text)
+ 
+
 
 if __name__ == '__main__':
-    
-    r_dir = './data/yuho/2016/'
-    w_dir = './data/text/2016/'
-    roop_html2text(r_dir, w_dir)
+    # 全期間
+    # r_dir = './data'
+    # html2text_all(r_dir)
 
-    # sample
-    # r_dir = './data/yuho_sample/2019/'
-    # w_dir = './data/text_sample/2019/'
-    # roop_html2text(r_dir, w_dir)
+    # １年間
+    base_dir = './data'
+    year = '2020'
+    html2text_year(base_dir, year)
